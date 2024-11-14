db25d3dbd7a318f9a7505134507305fa13a3181d
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-11-14 14:28:14 +0600

    userModel & .env added

---
+++ models/user.model.ts
@@ -0,0 +1,52 @@
+import { Document, Model, model, models, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  isVerified: boolean;
+  isAdmin: boolean;
+  forgotPasswordToken: string;
+  forgotPasswordTokenExpiry: Date;
+  verifyToken: string;
+  verifyTokenExpiry: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide an username"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+const UserModel: Model<IUser> = models.User || model<IUser>("User", UserSchema);
+
+export default UserModel;
