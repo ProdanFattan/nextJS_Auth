17a9b34b3e1dc4e45b86538ba962a5e074e9f821
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-11-14 15:05:26 +0600

    nodemailer setup and db connect

---
+++ dbConfig/dbConnect.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export default async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL || ""); //process.env.MONGO_URL!  etao dite partam
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("Connected to MongoDB");
+    });
+    connection.on("error", (err) => {
+      console.error("Error connecting to MongoDB: " + err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went wrong while connecting to DB");
+    console.log(error);
+  }
+}
