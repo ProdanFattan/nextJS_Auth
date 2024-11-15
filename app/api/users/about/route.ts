dea541aec00a96a99e2f4af6d8c3a037d19a204b
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-11-15 12:22:31 +0600

    about me backend

--- app/api/users/about/route.ts
+++ app/api/users/about/route.ts
@@ -3,12 +3,22 @@ import UserModel from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
 
 connect();
 
 export async function POST(request: NextRequest) {
   try {
     //extract data from token
+    const userId = await getDataFromToken(request);
+    const user = await UserModel.findOne({ _id: userId }).select("-password");
+    if (!user) {
+      return new NextResponse("Invalid token", { status: 400 });
+    }
+    return NextResponse.json({
+      message: "User found",
+      data: user,
+    });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
