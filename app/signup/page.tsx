a54a9212878c3822836c7fd391968f07928d26e8
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-11-15 15:42:51 +0600

    signup frontend functionality

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+export default function SignupPage() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default function page() {
-  return (
-    <div>page</div>
-  )
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success ", response);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed");
+      toast.error(error.message);
+    }
+  };
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.name.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  return <div>page</div>;
 }
