c8fad139b6a07df855f05608a1abbfcb7ad7423a
Md Fattan Prodan Pathul <prodanfattan@gmail.com>
2024-11-15 11:53:18 +0600

    logout backend

---
+++ app/api/users/logout/route.ts
@@ -0,0 +1,23 @@
+import connect from "@/dbConfig/dbConnect";
+import UserModel from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const response = NextResponse.json({
+      message: "Logout successfully",
+      success: true,
+    });
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
